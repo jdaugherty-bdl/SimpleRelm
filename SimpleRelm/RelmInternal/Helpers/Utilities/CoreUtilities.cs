ef49b446117cfaf0421239e406848b67622e6f85
jdaugherty-bdl <jdaugherty@bostondatalabs.com>
2025-03-17 16:09:54 -0400

    better scalar conversion for number types

--- SimpleRelm/RelmInternal/Helpers/Utilities/CoreUtilities.cs
+++ SimpleRelm/RelmInternal/Helpers/Utilities/CoreUtilities.cs
@@ -32,7 +32,7 @@ namespace SimpleRelm.RelmInternal.Helpers.Utilities
                 return decimal.TryParse(scalarValue.ToString(), out decimal scalar) ? scalar : default;
             else if (typeof(T) == typeof(float))
                 return float.TryParse(scalarValue.ToString(), out float scalar) ? scalar : default;
-            else if (typeof(T) == typeof(bool) && scalarValue.GetType() == typeof(string))
+            else if (typeof(T) == typeof(bool) && new[] { typeof(string), typeof(int), typeof(long) }.Contains(scalarValue.GetType()))
                 return !(scalarValue.ToString() == "0");
             else if (typeof(T) == typeof(DateTime))
                 return DateTime.TryParse(scalarValue.ToString(), out DateTime scalar) ? scalar : default;
