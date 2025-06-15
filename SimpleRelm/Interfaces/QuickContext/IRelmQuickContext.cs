f234634ac6ef4d4a1fcf92c523b9c3dbd00850c9
jdaugherty-bdl <jdaugherty@bostondatalabs.com>
2025-06-15 10:31:02 -0400

    update quick namespace

--- SimpleRelm/Interfaces/QuickContext/IRelmQuickContext.cs
+++ SimpleRelm/Interfaces/QuickContext/IRelmQuickContext.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SimpleRelm.Interfaces.QuickContext
+namespace SimpleRelm.Interfaces.RelmQuick
 {
     public interface IRelmQuickContext
     {
