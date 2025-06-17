585253ae4917653a130837af21f6a76068ded35f
jdaugherty-bdl <jdaugherty@bostondatalabs.com>
2025-06-17 17:34:22 -0400

    implement quick context

--- SimpleRelm/RelmInternal/Helpers/DataTransfer/DataLoaderHelper.cs
+++ SimpleRelm/RelmInternal/Helpers/DataTransfer/DataLoaderHelper.cs
@@ -1,5 +1,6 @@
 ﻿using SimpleRelm.Attributes;
 using SimpleRelm.Interfaces;
+using SimpleRelm.Interfaces.RelmQuick;
 using SimpleRelm.Options;
 using SimpleRelm.RelmInternal.Helpers.Utilities;
 using System;
@@ -17,7 +18,20 @@ namespace SimpleRelm.RelmInternal.Helpers.DataTransfer
     internal class DataLoaderHelper<T> where T : IRelmModel, new()
     {
         private readonly ICollection<T> targetObjects;
+        private readonly IRelmQuickContext relmQuickContext;
         private readonly IRelmContext relmContext;
+
+        public DataLoaderHelper(IRelmQuickContext relmContext, T targetObject)
+        {
+            this.targetObjects = new[] { targetObject };
+            this.relmQuickContext = relmContext;
+        }
+
+        public DataLoaderHelper(IRelmQuickContext relmContext, ICollection<T> targetObjects)
+        {
+            this.targetObjects = targetObjects;
+            this.relmQuickContext = relmContext;
+        }
 
         public DataLoaderHelper(IRelmContext relmContext, T targetObject)
         {
@@ -36,10 +50,13 @@ namespace SimpleRelm.RelmInternal.Helpers.DataTransfer
             var referenceProperty = predicate.Body as MemberExpression
                 ?? throw new InvalidOperationException("Collection or property must be represented by a lambda expression in the form of 'x => x.PropertyName'.");
 
-            var dataLoaderAttribute = referenceProperty.Member.GetCustomAttribute<RelmDataLoader>()
+            var dataLoaderAttribute = referenceProperty.Member.GetCustomAttributes<RelmDataLoader>()
+                ?.FirstOrDefault(y => y.LoaderType?.GetInterface(relmContext == null ? nameof(IRelmQuickFieldLoader) : nameof(IRelmFieldLoader)) != null)
                 ?? throw new MemberAccessException($"The property or collection [{referenceProperty.Member.Name}] on type [{referenceProperty.Expression.Type.Name}] does not have a RelmDataLoader attribute.");
 
-            var fieldLoader = (IRelmFieldLoader)Activator.CreateInstance(dataLoaderAttribute.LoaderType, new object[] { relmContext, referenceProperty.Member.Name, dataLoaderAttribute.KeyFields });
+            var fieldLoader = relmContext == null
+                ? (IRelmFieldLoader)Activator.CreateInstance(dataLoaderAttribute.LoaderType, new object[] { relmQuickContext, referenceProperty.Member.Name, dataLoaderAttribute.KeyFields })
+                : (IRelmFieldLoader)Activator.CreateInstance(dataLoaderAttribute.LoaderType, new object[] { relmContext, referenceProperty.Member.Name, dataLoaderAttribute.KeyFields });
 
             new FieldLoaderHelper<T>(targetObjects).LoadData(fieldLoader);
 
